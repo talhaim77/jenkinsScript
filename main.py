6387bbf076c316fa2931c93638589ef4e5267dda
talhaim <talhaim77@gmail.com>
2021-03-21 20:01:15 +0200

    commit2

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-
+# Hi user! print function
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('User! welcome to my github account :)')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
